6f9e0a09fc163e53ebe35f749849f15e8cf6094f
Marian Spoiala <marian.spoiala@gmail.com>
2020-10-12 04:56:51 +0300

    start using Routing module

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require("swagger-jsdoc");
 
 var config = require('./config/config.json');
-var routing = require('./routing');
+var peopleRouting = require('./routings/people_routing');
 
 var app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-routing.config(app);
+app.use('/people', peopleRouting);
 
 const options = {
     definition: {
@@ -31,17 +31,13 @@ const options = {
         },
       ],
     },
-    apis: ["routing.js"],
+    apis: ["./routings/people_routing.js"],
     explorer: true,
     swaggerOptions: {
         urls: [
           {
-            url: 'http://petstore.swagger.io/v2/swagger.json',
+            url: `http://localhost:${config.port}/swagger.json`,
             name: 'Spec1'
-          },
-          {
-            url: 'http://petstore.swagger.io/v2/swagger.json',
-            name: 'Spec2'
           }
         ]
       },
@@ -49,12 +45,7 @@ const options = {
   
   const specs = swaggerJsdoc(options);
 
-  app.use("/swagger", swaggerUi.serve, swaggerUi.setup(specs));
-  app.use('/api-docs', function(req, res, next){
-    swaggerDocument.host = req.get('host');
-    req.swaggerDoc = swaggerDocument;
-    next();
-}, swaggerUi.serve, swaggerUi.setup());
+  app.use("/swagger", swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }));
 
 app.listen(config.port, () => {
     console.log(`Example app listening on port ${config.port}!`);
