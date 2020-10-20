4971535ec58259354fc76afade9b8e87d70e8189
Marian Spoiala <marian.spoiala@gmail.com>
2020-10-21 00:53:57 +0300

    Moved swagger config to its own file

--- src/app.js
+++ src/app.js
@@ -1,52 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require("swagger-jsdoc");
-
+var swg = require('./swagger_config');
 var config = require('./config/config.json');
-
 var peopleRouting = require('./routings/people_routing');
 
 var app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+swg(app);
 app.use('/people', peopleRouting);
-
-const options = {
-    definition: {
-      openapi: "3.0.0",
-      info: {
-        title: "People Manager",
-        version: "0.1.0",
-        description:
-          "People Manager",
-        license: {
-          name: "MIT",
-          url: "https://spdx.org/licenses/MIT.html",
-        }
-      },
-      servers: [
-        {
-          url: "http://localhost:3000/",
-        },
-      ],
-    },
-    apis: ["./routings/people_routing.js"],
-    explorer: true,
-    swaggerOptions: {
-        urls: [
-          {
-            url: `http://localhost:${config.port}/swagger.json`,
-            name: 'Spec1'
-          }
-        ]
-      },
-  };
-  
-  const specs = swaggerJsdoc(options);
-
-  app.use("/swagger", swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }));
 
 app.listen(config.port, () => {
     console.log(`Example app listening on port ${config.port}!`);
---
+++ src/swagger_config.js
@@ -0,0 +1,42 @@
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require("swagger-jsdoc");
+var config = require('./config/config.json');
+
+module.exports = function AddSwagger(app) {
+    
+const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "People Manager",
+        version: "0.1.0",
+        description:
+          "People Manager",
+        license: {
+          name: "MIT",
+          url: "https://spdx.org/licenses/MIT.html",
+        }
+      },
+      servers: [
+        {
+          url: "http://localhost:3000/",
+        },
+      ],
+    },
+    apis: ["./routings/people_routing.js"],
+    explorer: true,
+    swaggerOptions: {
+        urls: [
+          {
+            url: `http://localhost:${config.port}/swagger.json`,
+            name: 'Spec1'
+          }
+        ]
+      },
+  };
+  
+  const specs = swaggerJsdoc(options);
+
+  app.use("/swagger", swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }));
+}
