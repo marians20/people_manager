f6629b41578cd6a9bbf27c4ffd5bcd5344620f2d
Marian Spoiala <marian.spoiala@gmail.com>
2020-10-21 06:59:59 +0300

    Added cors

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 process.env["NODE_CONFIG_DIR"] = __dirname + "\\config";
-var express = require('express');
-var bodyParser = require('body-parser');
-var swg = require('./swagger_config');
+const express = require('express');
+const bodyParser = require('body-parser');
+const swg = require('./swagger_config');
 const config = require('config');
-var peopleRouting = require('./routings/people_routing');
+const peopleRouting = require('./routings/people_routing');
+const app = express();
+var cors = require('cors');
 
-var app = express();
+app.use(cors());
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 swg(app);
 app.use('/people', peopleRouting);
-// var env = process.env.NODE_ENV || 'development'
-// console.log(`Environment is ${env}`);
 app.listen(config.port, () => {
     console.log(`Example app listening on port ${config.port}!`);
 });
