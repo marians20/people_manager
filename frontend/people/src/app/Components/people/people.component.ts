a616536a913d709cecbe190bbd30ea778ebc710c
Marian Spoiala <marian.spoiala@gmail.com>
2020-10-28 00:34:20 +0200

    Started sorting and pagination preparations.

--- frontend/people/src/app/Components/people/people.component.ts
+++ frontend/people/src/app/Components/people/people.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { merge, of as observableOf } from 'rxjs';
+import { catchError, map, startWith, switchMap } from 'rxjs/operators';
 import { People } from 'src/app/models';
-import { RestService } from './../../data-adapters/rest.service';
 import { PeopleDataSource } from './people.datasource';
 
 @Component({
@@ -8,17 +11,43 @@ import { PeopleDataSource } from './people.datasource';
   templateUrl: './people.component.html',
   styleUrls: ['./people.component.scss']
 })
-export class PeopleComponent implements OnInit {
+export class PeopleComponent implements OnInit, AfterViewInit  {
   public people: People;
+  private isLoadingResults: boolean;
+  private isRateLimitReached: boolean;
 
   displayedColumns: string[] = ['firstName', 'lastName', 'cnp'];
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
   constructor(
-    private rest: RestService,
     public dataSource: PeopleDataSource) {
   }
 
-  ngOnInit(): void {
-    this.dataSource.load();
+  ngAfterViewInit(): void {
+    console.log(this.sort.active, this.sort.direction, this.paginator.pageIndex);
+    this.dataSource.load(this.sort.active, this.sort.direction, this.paginator.pageIndex).subscribe();
+    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+    merge(this.sort.sortChange, this.paginator.page)
+    .pipe(
+      startWith({}),
+      switchMap(() => {
+        this.isLoadingResults = true;
+        return this.dataSource.load(this.sort.active, this.sort.direction, this.paginator.pageIndex);
+      }),
+      map(data => {
+        this.isLoadingResults = false;
+        return data;
+      }),
+      catchError(() => {
+        this.isLoadingResults = false;
+        // Catch if the GitHub API has reached its rate limit. Return empty data.
+        this.isRateLimitReached = true;
+        return observableOf([]);
+      })
+    ).subscribe();
   }
+
+  ngOnInit(): void { }
 }
--- frontend/people/src/app/Components/people/people.datasource.ts
+++ frontend/people/src/app/Components/people/people.datasource.ts
@@ -1,8 +1,10 @@
-import { Injectable, Type } from '@angular/core';
-import { Person } from 'src/app/models';
+import { Injectable } from '@angular/core';
+import { People, Person } from 'src/app/models';
 import { RestService } from './../../data-adapters/rest.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { EnvironmentService } from 'src/app/environment.service';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PeopleDataSource extends MatTableDataSource<Person> {
@@ -13,7 +15,11 @@ export class PeopleDataSource extends MatTableDataSource<Person> {
         rest.baseUrl = `${env.getValue('serverUrl')}/${env.getValue('peopleUrl')}`;
     }
 
-    public load(): void {
-        this.rest.get<Person>().subscribe(data => this.data = data);
+    public load(sortActive: string, sortDirection: string, pageIndex: number): Observable<People> {
+        return this.rest.get<Person>().pipe(
+            map(data => {
+                this.data = data;
+                return data;
+            }));
     }
 }
